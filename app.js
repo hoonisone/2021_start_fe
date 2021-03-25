4501b7c082cd0edcbc1b09e4de1776e072d5fe69
Myounghun <hoonisone@gmail.com>
2021-03-25 17:01:53 +0900

    crate 03 dir

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('app.js');
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+array.forEach(function(element1) {
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    array.forEach(function(element2) {
+        console.log(String.format("%d X %d = %d\n", element1, element2, element1*element2 ));
+    });
+});
