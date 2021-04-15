c787ab7ce8581347dc464d3b7aee40ceb6289dd6
Myounghun <hoonisone@gmail.com>
2021-04-15 21:16:28 +0900

    quiz4

---
+++ 05/quiz4.js
@@ -0,0 +1,25 @@
+const box = document.getElementById('box');
+
+box.addEventListener('mousedown', downHanddler);
+
+function downHanddler(e){
+    var preMouseX = e.clientX;
+    var preMouseY = e.clientY;
+
+    element = e.currentTarget;
+    element.addEventListener('mousemove', moveHanddler);
+    element.addEventListener('mouseup', upHanddler);
+
+    function moveHanddler(e){
+        element.style.top = (element.getBoundingClientRect().top + (e.clientY - preMouseY)) +"px";
+        element.style.left = (element.getBoundingClientRect().left + (e.clientX - preMouseX)) +"px";
+        preMouseX = e.clientX;
+        preMouseY = e.clientY;
+    }
+
+    function upHanddler(e){
+        element.removeEventListener('mousemove', moveHanddler);
+        element.removeEventListener('mouseup', upHanddler);
+    }
+}
+
